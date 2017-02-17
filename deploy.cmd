2b03d9f86e702813e12ff02142db15dac0147002
Leandro Ribeiro <bitbucket@leandroribeiro.com>
2017-02-17 18:27:20 -0200

    Atualizando script

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,4 @@
 @echo off 
 echo ---Deploying site
 REM ---Deploy the wwwroot folder in repository to default target (wwwroot)
-xcopy %DEPLOYMENT_SOURCE%\webapp\* %DEPLOYMENT_TARGET%/Y /s
+xcopy %DEPLOYMENT_SOURCE%\repository\webapp\* %DEPLOYMENT_TARGET%/Y /s
