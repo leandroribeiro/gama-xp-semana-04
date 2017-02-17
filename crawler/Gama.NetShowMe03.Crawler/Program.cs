8805d61c69d8302c9875751339398f079e9290ae
Leandro Ribeiro <bitbucket@leandroribeiro.com>
2017-02-17 18:10:08 -0200

    Removendo comentários

--- crawler/Gama.NetShowMe03.Crawler/Program.cs
+++ crawler/Gama.NetShowMe03.Crawler/Program.cs
@@ -32,11 +32,9 @@ namespace Gama.NetShowMe03.Crawler
 
 			var listaDeSites = new List<String>();
 
-			//listaDeSites.Add("http://www.leandroribeiro.com");
 			listaDeSites.Add("https://netshowme03.azurewebsites.net/transmissao.html?id=10");
 			listaDeSites.Add("http://netshow.me");
 			listaDeSites.Add("https://www.brightcove.com/en/");
-
 			listaDeSites.Add("https://www2.deloitte.com/br/pt.html");
 			listaDeSites.Add("https://www.sas.com/pt_br/home.html");
 			listaDeSites.Add("http://www.tcs.com/worldwide/br/pt/Pages/default.aspx");
@@ -62,12 +60,8 @@ namespace Gama.NetShowMe03.Crawler
 
 			var listaDeParametros = new List<String>();
 
-			//< video class="jw-video jw-reset" disableremoteplayback="" webkit-playsinline="" playsinline="" preload="auto" jw-loaded="data" src="//content.jwplatform.com/manifests/Bo2H0CpE.m3u8" x-webkit-wirelessvideoplaybackdisabled="" jw-played=""></video>
-				
-			//listaDeParametros.Add(@"<title>\s*(.+?)\s*");
 			listaDeParametros.Add(@"<video\s*(.+?)\s*");
 			listaDeParametros.Add(@"(?<=<iframe[^>]*?)(?:\s*width=[""'](?<width>[^""']+)[""']|\s*height=[""'](?<height>[^'""]+)[""']|\s*src=[""'](?<src>[^'""]+[""']))+[^>]*?>");
-			//listaDeParametros.Add(@"(?<=<video[^>]*?)[""']|\s*src=[""'](?<src>[^'""]+[""']))+[^>]*?>");
 
 			foreach (var site in listaDeSites)
 			{
@@ -134,43 +128,3 @@ namespace Gama.NetShowMe03.Crawler
 		}
 	}
 }
-
-/*
- * 
- * 
- * 
- * https://www2.deloitte.com/br/pt.html
-https://www.sas.com/pt_br/home.html
-http://www.tcs.com/worldwide/br/pt/Pages/default.aspx
-http://www.valeoservice.com.br/
-http://atento.com/pt/
-https://www.avanade.com/pt-br
-http://www.bb.com.br/pbb/pagina-inicial#/
-http://www.corbion.com/
-http://www.cyrela.com.br/sp
-http://www.dhl.com.br/pt.html
-http://www2.dimensiondata.com/pt-br
-http://www.everis.com/brazil/pt-BR/home/Paginas/home.aspx
-http://www.kimberly-clark.com.br/Novo/
-https://www.klabin.com.br/pt/home/
-http://www.bbmapfre.com.br/Default.aspx
-http://www.orange-business.com/en/brazil
-http://www.iniciobrilhanteoxiteno.com.br/
-https://www.pwc.com.br/
-http://www.repsolsinopec.com.br/
-http://www.saint-gobain.com.br/
-http://www.samsung.com/br/
-http://www.sirona.com.br/br/
-
- */
-
-/*
-
-//src="https://www.facebook.com/plugins/video.php?
-
-<div class="youtube componente_materia"><iframe allowfullscreen="true" allowtransparency="true" frameborder="0" height="350" scrolling="no" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fdelilah.taylor%2Fvideos%2F10207678174697366%2F&amp;show_text=0&amp;width=400" style="border:none;overflow:hidden" width="560"></iframe></div>
-
-
-<iframe src="https://www.youtube.com/embed/JUmH2-k6iq4" width="560" height="315" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
-
- */
